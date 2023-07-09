a4468b82f22e6f8a5beabe6e35869913d88815a2
GOOSxE <der.goose.4308@gmail.com>
2023-07-09 21:22:38 +0200

    completed hw-04-feedback

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,56 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Statistics from './Statistics/Statistics';
 import FeedbackOptions from './Buttons/Buttons';
 import Section from './Section/Section';
 import Notification from './Notification/Notification';
 // ? // Компонент класу зі своїм стейтом, в залежності від якого працюють інші компоненти, що знаходяться в середині нього ;
-export class App extends React.Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+export const App = props => {
+  let [good, setGood] = useState(0);
+  let [neutral, setNeutral] = useState(0);
+  let [bad, setBad] = useState(0);
+
   // ? // Універсальна функція, що при натисканні на кнопку оновлює стейт екземпляра класу ;
-  onLeaveFeedback = event => {
-    this.setState(prevState => {
-      return { [event.target.name]: (prevState[event.target.name] += 1) };
-    });
+  const onLeaveFeedback = event => {
+    if (event.target.name === 'good') {
+      setGood((good += 1));
+    } else if (event.target.name === 'neutral') {
+      setNeutral((neutral += 1));
+    } else if (event.target.name === 'bad') {
+      setBad((bad += 1));
+    }
   };
   // ? // Підрахунок кількості відгуків ;
-  countTotalFeedback = (good, neutral, bad) => {
+  const countTotalFeedback = (good, neutral, bad) => {
     return good + neutral + bad;
   };
   // ? // Процентне відношення позитивних відгуків ;
-  countPositiveFeedbackPercentage = (good, neutral, bad) => {
+  const countPositiveFeedbackPercentage = (good, neutral, bad) => {
     return Math.round((good * 100) / (good + neutral + bad));
   };
   // ? // Рендер усіх компонентів ;
-  render() {
-    const { good, neutral, bad } = this.state;
-    return (
-      <div className="App">
-        <Section title="Please, leave your feedback!">
-          <FeedbackOptions
-            options={Object.keys(this.state)}
-            onLeaveFeedback={this.onLeaveFeedback}
-          ></FeedbackOptions>
-        </Section>
-        <Section title="Statistics">
-          {good || neutral || bad ? (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              countTotalFeedback={this.countTotalFeedback}
-              countPositiveFeedbackPercentage={
-                this.countPositiveFeedbackPercentage
-              }
-            ></Statistics>
-          ) : (
-            <Notification message="There is no feedback"></Notification>
-          )}
-        </Section>
-      </div>
-    );
-  }
-}
+  const optionsObject = { good, neutral, bad };
+  return (
+    <div className="App">
+      <Section title="Please, leave feedback!">
+        <FeedbackOptions
+          options={Object.keys(optionsObject)}
+          onLeaveFeedback={onLeaveFeedback}
+        ></FeedbackOptions>
+      </Section>
+      <Section title="Statistics">
+        {good || neutral || bad ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            countTotalFeedback={countTotalFeedback}
+            countPositiveFeedbackPercentage={countPositiveFeedbackPercentage}
+          ></Statistics>
+        ) : (
+          <Notification message="There is no feedback"></Notification>
+        )}
+      </Section>
+    </div>
+  );
+};
