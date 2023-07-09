a8be9a165f7e32e961edc69089d5c1a975602c37
GOOSxE <der.goose.4308@gmail.com>
2023-07-09 21:47:15 +0200

    Update Buttons.jsx

--- src/components/Buttons/Buttons.jsx
+++ src/components/Buttons/Buttons.jsx
@@ -2,23 +2,21 @@ import React from 'react';
 import propTypes from 'prop-types';
 import css from './Buttons.module.css';
 // ? // Компонент кнопок що приймає масив з опціями для рендеру, та функцію для обробки кліків ;
-const FeedbackOptions = ({ options, onLeaveFeedback }) => {
-  return (
-    <ul className={css.options_list}>
-      {options.map((option, index) => (
-        <li className={css.options_list_item} key={index}>
-          <button
-            className={css.options_list_button}
-            onClick={onLeaveFeedback}
-            name={option}
-          >
-            {option.replace(option[0], option[0].toUpperCase())}
-          </button>
-        </li>
-      ))}
-    </ul>
-  );
-};
+const FeedbackOptions = ({ options, onLeaveFeedback }) => (
+  <ul className={css.options_list}>
+    {options.map((option, index) => (
+      <li className={css.options_list_item} key={index}>
+        <button
+          className={css.options_list_button}
+          onClick={onLeaveFeedback}
+          name={option}
+        >
+          {option.replace(option[0], option[0].toUpperCase())}
+        </button>
+      </li>
+    ))}
+  </ul>
+);
 FeedbackOptions.propTypes = {
   options: propTypes.array.isRequired,
   onLeaveFeedback: propTypes.func.isRequired,
